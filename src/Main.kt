e251a96012561bd4ea31968794186581df598268
miskaldukhi <miskaldukhi@gmail.com>
2025-02-18 14:32:19 +0300

    counter

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,20 @@
 fun main() {
-    println("Hello!")
+    println("Upwards:")
+    (countUpwards(5,10))
+    println("\nDownwards:")
+    countDownwards(5,1)
+
+
+}
+fun countUpwards(start: Int, end: Int){
+for(i in start..end){
+println(i)
+    }
+}
+fun countDownwards(end: Int, start: Int){
+    var i= end
+    while(i >= start) {
+        println(i)
+        i--
+    }
 }
